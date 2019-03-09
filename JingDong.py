31e72ffb35f9e65c1e3308fa47310c00cd608085
XuShengYuu <45997837+XuShengYuu@users.noreply.github.com>
2019-03-09 16:42:22 +0800

    create JingDong.py
    fix bug

--- JingDong.py
+++ JingDong.py
@@ -1,92 +1,155 @@
+import time
 from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-from pyquery import PyQuery as py
-import re
+from lxml import etree
 import pymongo
 from config import *
-import time
 
 client = pymongo.MongoClient(MONGO_URL)
 db = client[MONGO_DB]
 
 browser = webdriver.Chrome()
-wait =  WebDriverWait(browser,10)
+wait = WebDriverWait(browser,50)
+'''
+PhantomJS浏览器配置，采集速度更快
+# browser = webdriver.PhantomJS(service_args=SERVICE_ARGS)
+# browser.set_window_size(1400, 900)
+'''
 
 def search():
+    browser.get('https://www.jd.com/')
     try:
-        browser.get('https://www.jd.com/')
-        input = wait.until(
-            EC.presence_of_element_located((By.CSS_SELECTOR,"#key")))
-        submit = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,'#search > div > div.form > button')))
-        input.send_keys('牛奶')
+        input = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR,"#key")))
+        # 搜索框
+        submit = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,"#search > div > div.form > button")))
+        # 搜索确定按钮
+        input[0].send_keys('KETWORD')
+        # 输入关键词
         submit.click()
-        time.sleep(5)
-        length = 1200
-        for i in range(0,4):
+        # 点击确定搜索关键词
+        time.sleep(10)
+        # 暂停10s，等待信息加载完毕
+        '''
+        将浏览器逐渐下拉，加载出图片信息
+        如果直接下拉到底，部分图片链接会加载不出
+        如果不需要图片信息，可用下面另一种方法
+        '''
+        length = 1000
+        for i in range(0,10):
             js="var q=document.documentElement.scrollTop="+str(length)
+            length =length+1000
             browser.execute_script(js)
-            length += length
-        # browser.execute_script('window.scrollTo(0, document.body.scrollHeight)')
-            # i += 1
-            time.sleep(3)
-        total = wait.until(
-            EC.presence_of_element_located((By.CSS_SELECTOR,"##J_bottomPage > span.p-skip > em:nth-child(1) > b")))
-        get_goods()
-        return total.text
+            i += 1
+            time.sleep(1)
+        '''
+        此方法适用不需要加载图片信息
+        time.sleep(3)
+        # 暂停3秒，等待网页内容加载完毕
+        browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        # 下拉浏览器，加载后30个商品内容
+        time.sleep(5)
+        # 暂停3秒，等待网页加载完毕
+        '''
+        total = wait.until(EC.presence_of_all_elements_located
+            ((By.CSS_SELECTOR,'#J_bottomPage > span.p-skip > em:nth-child(1) > b')))
+        # 定位总页码数元素
+        html = browser.page_source
+        # 取得网页源码
+        get_goods(html)
+        # 执行get_goods函数，获取第一页商品信息
+        return total[0].text
+        # 返回总页面数
     except TimeoutException:
-        return search()
+        search()
 
-def get_next_page(page_number):
+def next_page(page_number):
     try:
-        input = wait.until(
-            EC.presence_of_element_located((By.CSS_SELECTOR,"#J_bottomPage > span.p-skip > input"))
-        )
-        submit = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,'#J_bottomPage > span.p-skip > a')))
-        input.clear()
-        input.send_keys(page_number)
-        submit.click()
-        wait.until(EC.text_to_be_present_in_elemen((By.CSS_SELECTOR,'#J_bottomPage > span.p-num > a:nth-child(3)'),str(page_number)))
-        get_goods()
+        button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,'#J_bottomPage > span.p-num > a.pn-next > em')))
+        # 翻页按钮
+        button.click()
+        # 点击翻页按钮
+        '''
+        将浏览器逐渐下拉，加载出图片信息
+        如果直接下拉到底，部分图片链接会加载不出
+        如果不需要图片信息，可用下面另一种方法
+        '''
+        length = 1000
+        for i in range(0,10):
+            js="var q=document.documentElement.scrollTop="+str(length)
+            length =length+1000
+            browser.execute_script(js)
+            i += 1
+            time.sleep(1)
+        '''
+        此方法适用不需要加载图片信息
+        time.sleep(3)
+        # 暂停3秒，等待网页内容加载完毕
+        browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        # 下拉浏览器，加载后30个商品内容
+        time.sleep(5)
+        # 暂停3秒，等待网页加载完毕
+        '''
+        wait.until(EC.text_to_be_present_in_element
+            ((By.CSS_SELECTOR,"#J_bottomPage > span.p-num > a.curr"),str(page_number)))
+        # 判断是否翻页成功
+        html = browser.page_source
+        get_goods(html)
+        # 执行get_goods函数，获取商品信息
     except TimeoutException:
-        next_page(page_number)
+        return next_page(page_number)
 
-def get_goods():
-    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,'#m-list .J_goodsList .gl-item')))
-    html = browser.page_soure
-    doc = py(html)
-    item = doc('#m-list .J_goodsList .gl-item').items()
+def get_goods(html):
+    html = etree.HTML(html)
+    # 解析网页
+    items = html.xpath('//*[@id="J_goodsList"]/ul/li')
+    # print(items)    
+    i = 0
+    # 初始化计数
     for item in items:
+        # 取出单个商品信息
+        print("第",i+1,"个商品")
+        # 统计商品次序
         good = {
-            'image':item.find('.p-img img').attr('src'),
-            'price':item.find('.p-price ').text(),
-            'title':item.find('.p-name p-name-type-2').text(),
-            'shop':item.find('.p-shop').text()
-        }
+        # 单个商品信息
+            'href': item.xpath("./div/div[1]/a/@href"),
+            # 商品链接
+            'image': item.xpath("./div/div[1]/a/img/@src"),
+            # 商品图片
+            'price': item.xpath("./div/div[3]/strong/i/text()"),
+            # 商品价格
+            'title': item.xpath("./div/div[4]/a/em/text()"),
+            # 商品名称
+            'shop': item.xpath("./div/div[7]/span/a/text()")
+            # 商店名称
+        }       
+        i=i+1
+        # 计数+1
         print(good)
+        # 打印商品信息
         save_to_mongo(good)
+        # 储存商品信息到MongoDB
 
 def save_to_mongo(result):
     try:
         if db[MONGO_TABLE].insert(result):
-            print('存储到MONGODB成功', result)
+            #将商品信息储存到MongoDB表中
+            print('存储到MONGODB成功')
     except Exception:
-        print('存储到MONGODB失败', result)
+        print('存储到MONGODB失败')
 
-def main(): 
-    # search()
-    try:
-        total = search()
-        total = int(re.compile('\d+').search(total).group(1))
-        print(total)
-        for i in range(2,total+1):
-            next_page(i)
-    except Exception:
-        print('出错啦') 
-    finally:
-        browser.close()
+def main():
+    print("第",1,"页")
+    # 打印页码
+    total = int(search())
+    # 总页码数
+    for i in range(2, total+1):
+        # 循环页码数
+        time.sleep(3)
+        print("第",i,"页")
+        next_page(i)
 
 if __name__ == '__main__':
     main()
--- config.py
+++ config.py
@@ -1,3 +1,6 @@
 MONGO_URL = 'localhost'
 MONGO_DB = 'JingDong'
 MONGO_TABLE = 'goods'
+
+KETWORD = '牛奶'
+SERVICE_ARGS = ['--load-images=false', '--disk-cache=true']
